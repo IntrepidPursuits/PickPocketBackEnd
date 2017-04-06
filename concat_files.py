ab5635e91d0f7304400c163b78edfd18ec270e1e
John McCormack <john@intrepid.io>
2017-04-06 11:09:08 -0400

    Remove yaml files and start creating files from the configuration.json file

--- concat_files.py
+++ concat_files.py
@@ -13,57 +13,66 @@ SOURCE_ARN = {"Fn::Join" : ["", ["arn:aws:execute-api:", { "Ref" : "AWS::Region"
 REST_API_ID = {"Ref" : "RestApi"}
 LAMBDA_PERMISSION_ARN = {"Fn::Join" : ["", ["arn:aws:iam::", { "Ref" : "AWS::AccountId"}, ":role/pickPocketWriteAndSNS"]]}
 
-for file in listdir():
-    if file.endswith(".py") and file != "concat_files.py":
-        pythonFiles.append(file)
+with open('config.json') as configFile:
+    configJson = json.load(configFile)
 
 parentyaml = {}
 jsonOut = {}
+resourcePaths = []
+resourcePathExists = False
 
-for pythonFile in pythonFiles:
+for config in configJson['functions']:
     with open('deploy_template.json') as templateFile:
         with open('api_gateway_template.json') as apiTemplateFile:
-            with open(pythonFile) as appendFile:
-                functionName = pythonFile.replace(".py", "") + "CLOUDFORM"
+            functionName = config['function'].replace(".py", "") + "CLOUDFORM"
 
-                tempJSON = json.load(templateFile)
-                apiTemplateJSON = json.load(apiTemplateFile)
+            tempJSON = json.load(templateFile)
+            apiTemplateJSON = json.load(apiTemplateFile)
 
-                jsonOut[functionName] = tempJSON['FunctionName']
+            jsonOut[functionName] = tempJSON['FunctionName']
 
-                # Create Function
-                propJson = jsonOut[functionName]["Properties"]
-                codeJson = propJson["Code"]
-                codeJson['ZipFile'] = appendFile.read()
-                propJson['FunctionName'] = functionName
-                propJson['Role'] = LAMBDA_PERMISSION_ARN
+            # Create Function
+            propJson = jsonOut[functionName]["Properties"]
+            codeJson = propJson["Code"]
+            with open(config['function']) as pythonFile:
+                codeJson['ZipFile'] = pythonFile.read()
+            propJson['FunctionName'] = functionName
+            propJson['Role'] = LAMBDA_PERMISSION_ARN
 
-                # Add Permission
-                permissionTemplate = apiTemplateJSON[PERMISSION]
-                permissionName = PERMISSION + functionName
-                jsonOut[permissionName] = permissionTemplate
-                jsonOut[permissionName]['DependsOn'] = functionName
-                jsonOut[permissionName]['Properties']['FunctionName'] = functionName
-                jsonOut[permissionName]['Properties']['SourceArn'] = SOURCE_ARN
+            # Add Permission
+            permissionTemplate = apiTemplateJSON[PERMISSION]
+            permissionName = PERMISSION + functionName
+            jsonOut[permissionName] = permissionTemplate
+            jsonOut[permissionName]['DependsOn'] = functionName
+            jsonOut[permissionName]['Properties']['FunctionName'] = functionName
+            jsonOut[permissionName]['Properties']['SourceArn'] = SOURCE_ARN
 
-                # Add Resource
+            # Add Resource
+            if (config['api_path'] not in resourcePaths):
                 resourceTemplate = apiTemplateJSON[RESOURCE]
                 resourceName = RESOURCE + functionName
                 jsonOut[resourceName] = resourceTemplate
                 jsonOut[resourceName]['DependsOn'] = functionName
-                # TODO pull path name from other JSON file
-                jsonOut[resourceName]['Properties']['PathPart'] = functionName.replace("CLOUDFORM", "")
+                jsonOut[resourceName]['Properties']['PathPart'] = config['api_path']
+                resourcePaths.append(config['api_path'])
+                resourcePathExists = False
+            else:
+                # If the resource path is a repeat then we don't need to make two
+                resourcePathExists = True
 
-                # Add Method
-                # TODO add way to tell whether this should be a GET or a POST
-                methodTemplate = apiTemplateJSON[METHOD]
-                methodName = METHOD + functionName
-                jsonOut[methodName] = methodTemplate
-                methodUri = { "Fn::Join" : ["", ["arn:aws:apigateway", ":", { "Ref" : "AWS::Region" }, ":","lambda:path/2015-03-31/functions/arn:aws:lambda", ":", { "Ref" : "AWS::Region" }, ":", { "Ref" : "AWS::AccountId" }, ":function:", functionName, "/invocations"]]}
-                jsonOut[methodName]['Properties']['Integration']['Uri'] = methodUri
-                jsonOut[methodName]['Properties']['RestApiId'] = REST_API_ID
-                jsonOut[methodName]['Properties']['ResourceId'] = { "Ref": resourceName}
+            # Add Method
+            methodTemplate = apiTemplateJSON[METHOD]
+            methodName = METHOD + functionName
+            jsonOut[methodName] = methodTemplate
+            methodUri = { "Fn::Join" : ["", ["arn:aws:apigateway", ":", { "Ref" : "AWS::Region" }, ":","lambda:path/2015-03-31/functions/arn:aws:lambda", ":", { "Ref" : "AWS::Region" }, ":", { "Ref" : "AWS::AccountId" }, ":function:", functionName, "/invocations"]]}
+            jsonOut[methodName]['Properties']['Integration']['Uri'] = methodUri
+            jsonOut[methodName]['Properties']['RestApiId'] = REST_API_ID
+            jsonOut[methodName]['Properties']['ResourceId'] = { "Ref": resourceName}
+            if (resourcePathExists):
+                jsonOut[methodName]['DependsOn'] = [functionName, permissionName]
+            else:
                 jsonOut[methodName]['DependsOn'] = [functionName, resourceName, permissionName]
+            jsonOut[methodName]['Properties']['HttpMethod'] = config['method']
 
 with open("deploy.json", "a") as f:
     with open("api_gateway_base.json") as baseTemplateFile:
